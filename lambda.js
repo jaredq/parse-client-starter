c80e2c1d6aff25fcc3a8b3975c7928ae8d55dc4b
Jared Qin <jaredqin@gmail.com>
2019-08-29 15:28:21 +1000

    "ecmaVersion": 6

--- lambda.js
+++ lambda.js
@@ -7,3 +7,13 @@ const parseServerUrl = process.env.SERVER_URL || 'http://localhost:1337/parse';
 Parse.initialize(appID, javascriptKey, masterKey);
 Parse.Cloud.useMasterKey();
 Parse.serverURL = parseServerUrl;
+
+exports.handler = async event => {
+  // TODO implement
+  console.log('event', JSON.stringify(event));
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify('Hello from Lambda!')
+  };
+  return response;
+};
